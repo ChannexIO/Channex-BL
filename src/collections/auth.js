f9d744e0560cbc6d2abc918513f4bb9716e2148c
Andrew Yudin <pasarian@gmail.com>
2018-10-10 10:53:10 +0600

    Add Auth and Registration methods

--- src/collections/auth.js
+++ src/collections/auth.js
@@ -30,4 +30,35 @@ export default class Auth {
         return response;
       });
   }
+
+  whiteLabelSignUp(attrs) {
+    return this.transport
+      .send('POST', 'wl_sign_up', {user: attrs})
+      .then(response => {
+        if (response.data.attributes.token) {
+          this.transport.registerAccessToken(response.data.attributes.token);
+          this.storage.sessionAdd(response.data.attributes);
+        }
+
+        return response;
+      });
+  }
+
+  requestRestorePassword(email) {
+    return this.transport
+      .send('POST', 'request_restore_password', {user: {email: email}})
+      .then(response => response);
+  }
+
+  restorePassword(attrs) {
+    return this.transport
+      .send('POST', 'restore_password', {user: attrs})
+      .then(response => response);
+  }
+
+  confirmRegistration(token) {
+    return this.transport
+      .send('GET', `confirm_registration?token=${token}`)
+      .then(response => response);
+  }
 }
