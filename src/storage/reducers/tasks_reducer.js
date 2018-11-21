6d9193a24bc373a4806925c4a12f271bf67992b2
Andrew Yudin <pasarian@gmail.com>
2018-11-21 08:20:39 +0600

    Change logic for tasks reducer to reload entities

--- src/storage/reducers/tasks_reducer.js
+++ src/storage/reducers/tasks_reducer.js
@@ -1,6 +1,4 @@
-import {
-  TASKS_LOAD
-} from '../constants';
+import { TASKS_LOAD } from '../constants';
 
 const initialState = {
   entities: null,
@@ -8,23 +6,15 @@ const initialState = {
 };
 const ACTION_HANDLERS = {
   [TASKS_LOAD]: (state, action) => {
-    const entities = Object.assign(
-      {},
-      state.entities || {},
-      action.payload.tasks
-        .reduce((acc, el) => {
-          acc[el.id] = el.attributes;
-          if (el.relationships) {
-            Object.keys(el.relationships)
-              .forEach(
-                key => {
-                  acc[el.id][`${key}_id`] = el.relationships[key].data.id;
-                }
-              );
-          }
-          return acc;
-        }, {})
-    );
+    const entities = action.payload.tasks.reduce((acc, el) => {
+      acc[el.id] = el.attributes;
+      if (el.relationships) {
+        Object.keys(el.relationships).forEach(key => {
+          acc[el.id][`${key}_id`] = el.relationships[key].data.id;
+        });
+      }
+      return acc;
+    }, {});
 
     return {
       entities,
