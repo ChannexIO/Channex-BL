0f6ca921608a85c473be46a4141c74c622b01ca0
Andrew Yudin <pasarian@gmail.com>
2019-05-24 09:33:16 +0600

    Add cache for user

--- src/storage/reducers/user_reducer.js
+++ src/storage/reducers/user_reducer.js
@@ -2,7 +2,9 @@ import {
   USER_ADD
 } from '../constants';
 
-const initialState = null;
+const CACHE_KEY = 'CHANNEX_USER';
+const cache = localStorage.getItem(CACHE_KEY);
+const initialState = cache ? JSON.parse(cache) : null;
 const ACTION_HANDLERS = {
   [USER_ADD]: (state, action) => {
     return action.payload;
@@ -11,6 +13,9 @@ const ACTION_HANDLERS = {
 
 export default function usersReducer(state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type];
+  const updated_state = handler ? handler(state, action) : state;
 
-  return handler ? handler(state, action) : state;
+  localStorage.setItem(CACHE_KEY, JSON.stringify(updated_state));
+
+  return updated_state;
 }
